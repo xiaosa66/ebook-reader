84b4293fdfa731178bf16823790b67c56f59bdb3
Liuxs <xiao.sa@icloud.com>
2018-11-15 10:46:43 +0800

    添加epub展示

--- src/components/eBook.vue
+++ src/components/eBook.vue
@@ -1,11 +1,28 @@
 <template>
-<div>
-    eBook
+<div class="ebook-container">
+    <div id="read"></div>
 </div>
 </template>
 <script>
+import Epub from 'epubjs';
+global.Epub = Epub;
+const DOWNLOAD_URL = '/static/hongFuYeBen.epub';
 export default {
 name: 'eBook',
+methods:{
+    showEpub(){
+        this.book = new Epub(DOWNLOAD_URL);
+        // console.log('this.book',this.book);
+        this.rendition = this.book.renderTo('read',{
+            width:window.innerWidth,
+            height:window.innerHeight,
+        })
+        this.rendition.display();
+    }
+},
+mounted(){
+    this.showEpub();
+    }
 }
 </script>
 <style>
