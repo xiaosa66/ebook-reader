a63888a7a2eeb7dff762953f864e16d32513a95f
Liuxs <xiao.sa@icloud.com>
2018-11-15 23:07:38 +0800

    气死我了

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,21 @@
 export default {
   name: 'App'
 }
+document.addEventListener('DOMContentLoaded',()=>{
+  const html = document.querySelector('html');
+  let fontSize = window.innerWidth / 10;
+  fontSize = fontSize > 50 ? 50 : fontSize;
+  html.style.fontSize = fontSize + 'px';
+})
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
--- src/components/eBook.vue
+++ src/components/eBook.vue
@@ -1,6 +1,11 @@
 <template>
 <div class="ebook-container">
     <div id="read"></div>
+    <div class="mask">
+        <div class="left" v-on:click="prevPage"></div>
+        <div class="center"></div>
+        <div class="right" v-on:click="nextPage"></div>
+    </div>
 </div>
 </template>
 <script>
@@ -10,6 +15,17 @@ const DOWNLOAD_URL = '/static/hongFuYeBen.epub';
 export default {
 name: 'eBook',
 methods:{
+        prevPage(){
+        if(this.rendition){
+            this.rendition.prev();
+        }
+    },
+    nextpage(){
+        if(this.rendition){
+            this.rendition.next();
+            console.log('next')
+        }
+    },
     showEpub(){
         this.book = new Epub(DOWNLOAD_URL);
         // console.log('this.book',this.book);
@@ -18,12 +34,41 @@ methods:{
             height:window.innerHeight,
         })
         this.rendition.display();
-    }
+    },
+
 },
 mounted(){
     this.showEpub();
     }
 }
 </script>
-<style>
+<style lang='scss' scoped>
+@import '@/assets/styles/global.scss';
+.ebook-container {
+    position: relative;
+
+     .mask {
+        position: absolute;
+        display: flex;
+        top: 0;
+        left: 0;
+        width:100%;
+        height: 100%;
+        z-index: 100;
+        // background-color:green;
+    /*  */
+    /* background-color:rgba(0,0,0,0.5); */
+
+        .left{
+            flex:0 0 px2rem(100);
+        }
+        .center{
+            flex:1;
+        }
+        .right{
+            flex:0 0 px2rem(100);
+        }
+    }
+}
+
 </style>
