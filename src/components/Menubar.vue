5750e93a669128d4793089ac6e30b2e3c87591df
李瑞 <lirui@xiaoshiyideMacBook-Air.local>
2018-11-16 17:02:46 +0800

    添加font-setting

--- src/components/Menubar.vue
+++ src/components/Menubar.vue
@@ -1,13 +1,18 @@
 
 <template>
-        <transition name="slide-up">
-            <div class="menu-wrapper" v-show="ifTitleAndMenuSHow">
-                <div class="icon-wrapper">章节</div>
-                <div class="icon-wrapper">进度</div>
-                <div class="icon-wrapper">亮度</div>
-                <div class="icon-wrapper">字体</div>
+            <div class="menu-bar">
+                <transition name="slide-up">
+                    <div class="menu-wrapper" v-show="ifTitleAndMenuSHow">
+                        <div class="icon-wrapper">章节</div>
+                        <div class="icon-wrapper">进度</div>
+                        <div class="icon-wrapper">亮度</div>
+                        <div class="icon-wrapper">字体</div>
+                    </div>
+                </transition>
+                <div class="setting-wrapper">
+                    <div class="setting-font-size"></div>
+                </div>
             </div>
-        </transition>
 </template>
 
 <script>
@@ -20,23 +25,37 @@
 
 <style lang="scss" scoped>
 @import '../assets/styles/global';
-    .menu-wrapper {
-        color:#333;
-        font-size: 20px;
-        position: absolute;
-        display: flex;
-        box-shadow: 0 px2rem(-8) px2rem(8) rgba($color: #000000, $alpha: 0.15);
-        z-index: 101;
-        bottom:0;
-        left: 0;
-        width: 100%;
-        height: px2rem(60);
-        background: white;
-        .icon-wrapper {
-            @include center;
-            // flex: 0 0 px2rem(250);
-            flex: 1;
-            }
+    .menu-bar {  
+        .menu-wrapper {
+            color:#333;
+            font-size: 20px;
+            position: absolute;
+            display: flex;
+            box-shadow: 0 px2rem(-8) px2rem(8) rgba($color: #000000, $alpha: 0.15);
+            z-index: 101;
+            bottom:0;
+            left: 0;
+            width: 100%;
+            height: px2rem(60);
+            background: white;
+            .icon-wrapper {
+                @include center;
+                // flex: 0 0 px2rem(250);
+                flex: 1;
+                }
 
+        }
+        .setting-wrapper {
+            position: absolute;
+            bottom: px2rem(60);
+            left: 0;
+            width: 100%;
+            height: px2rem(80);
+            // background: #000000;
+            .setting-font-size {
+
+            }
+        }
     }
+
 </style>
